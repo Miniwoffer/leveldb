ebbc8977072cc5b32ae6cece6b00fce6a856ce79
Ingar Solveigson Asheim <isa@applica.no>
2026-06-24 09:16:51 +0200

    Over-engineered union version
    Relies on system-dependent behavior, so, while cool, it is not an
    especially robust design fit for prod

--- include/leveldb/error.h
+++ include/leveldb/error.h
@@ -1,18 +1,38 @@
 
+#include <array>
 #include <cassert>
-#include <concepts>
+#include <cstdint>
 #include <memory>
+#include <optional>
 #include <string>
-#include <string_view>
-#include <utility>
 
 #include "leveldb/export.h"
 
 namespace leveldb {
 
+namespace {
+constexpr const size_t PTR_SZ = sizeof(std::unique_ptr<std::string>);
+
+consteval auto EnumBaseType() {
+  if constexpr (PTR_SZ == 8) {
+    return uint_least32_t{};
+  } else {
+    return []() { static_assert(PTR_SZ == 8, "Unsupported pointer size"); }();
+  }
+}
+
+using enum_base_type = decltype(EnumBaseType());
+
+template <typename T>
+concept is_string_like =
+    std::same_as<std::string, T> || std::same_as<std::string_view, T> ||
+    std::same_as<const char*, T>;  // Unsure if we should allow const char*
+
+}  // namespace
+
 class LEVELDB_EXPORT Error {
  public:
-  enum ErrorCode {
+  enum class Code : enum_base_type {
     NotFound = 1,
     Corruption = 2,
     NotSupported = 3,
@@ -20,87 +40,90 @@ class LEVELDB_EXPORT Error {
     IOError = 5,
   };
 
-  Error(ErrorCode c) noexcept : code_(c) {}
-  ~Error() = default;
-  Error(const Error&) = default;
-  Error& operator=(const Error&) = default;
-  Error(const Error&& other) { *this = std::move(other); }
-  Error& operator=(const Error&& other) {
-    code_ = other.code_;
-    return *this;
-  }
-
-  std::string ToString() const;
-  ErrorCode code() const;
-  bool operator==(const ErrorCode rhs) const { return code() == rhs; }
-  bool operator==(const Error& rhs) const { return code() == rhs.code(); }
-
- private:
-  friend class DetailedError;
-  Error() = default;
-
-  ErrorCode code_{IS_DETAILED_ERROR_};
-  static const ErrorCode IS_DETAILED_ERROR_{static_cast<const ErrorCode>(0)};
-};
-
-template <typename T>
-concept is_string_like =
-    std::same_as<std::string, T> || std::same_as<std::string_view, T> ||
-    std::same_as<const char*, T>;  // Unsure if we should allow const char*
-
-class LEVELDB_EXPORT DetailedError : public Error {
- public:
-  // Status has support for 1 or 2 messsages, this can take an arbitrary
-  // number
   template <typename... Args>
     requires(is_string_like<Args> && ...)
-  DetailedError(ErrorCode c, Args... args) {
+  Error(Code c, Args... args) {
     std::string_view messages[] = {args...};
-    state_ = std::make_unique<std::string>();
-    state_->push_back(static_cast<char>(c));
+    string_ = std::make_unique<std::string>();
+    string_->push_back(static_cast<char>(c));
     for (const auto& m : messages) {
-      state_->append(m.data(), m.size());
-      state_->append(": ", 2);
+      string_->append(m.data(), m.size());
+      string_->append(": ", 2);
     }
-    state_->resize(state_->size() - 2);  // Remove last ": "
+    string_->resize(string_->size() - 2);  // Remove last ": "
   }
 
-  DetailedError(DetailedError&& other) : Error(std::move(other)) {
-    *this = std::move(other);
-  }
-  DetailedError& operator=(DetailedError&& other) {
-    if (this != &other) {
-      std::string* others_message = other.state_.release();
-      state_.reset(others_message);
+  Error(Code c) : code_(c) { pad_.back() = 1; }
+
+  ~Error() {
+    if (IsString()) {
+      string_.reset();
     }
-    return *this;
-  };
-
-  DetailedError(const DetailedError& other) : Error(other) { *this = other; }
-  DetailedError& operator=(const DetailedError& other) {
-    state_ = std::make_unique<std::string>(*other.state_);
-    return *this;
   }
 
-  ~DetailedError() = default;
+  Code code() const {
+    return IsString() ? static_cast<Code>((*string_)[0]) : code_;
+  }
 
-  ErrorCode code() const { return static_cast<ErrorCode>((*state_)[0]); }
+  bool operator==(const Code rhs) { return code() == rhs; }
+  bool operator==(const Error& rhs) { return code() == rhs.code(); }
+
+  std::string ToString() const {
+    std::string error_string;
+    switch (code()) {
+      case Code::NotFound:
+        error_string = "Not found";
+        break;
+      case Code::Corruption:
+        error_string = "Corruption";
+        break;
+      case Code::NotSupported:
+        error_string = "Not implemented";
+        break;
+      case Code::InvalidArgument:
+        error_string = "Invalid argument";
+        break;
+      case Code::IOError:
+        error_string = "IO error";
+        break;
+      default:
+        return "Invalid error code";
+    }
+
+    if (!IsString()) {
+      return error_string;
+    }
+
+    error_string.append(": ");
+    error_string.append(*Message());
+    return error_string;
+  }
 
  private:
-  friend class Error;
-  std::unique_ptr<std::string> state_{nullptr};
+  union {
+    std::unique_ptr<std::string> string_{nullptr};
+    struct {
+      Code code_;
+      std::array<char, PTR_SZ - sizeof(code_)> pad_;
+    };
+  };
 
-  std::string_view message() const {
-    return {&(*state_)[1], state_->size() - 1};
+  std::optional<std::string> Message() const {
+    if (IsString()) {
+      return string_->substr(1, string_->size() - 1);
+    }
+    return std::nullopt;
   }
+
+  // Relies on user space addresses always being in [0, 2^48), which means bytes
+  // 7 and 8 of a user space pointer will always be 0. This check will, in this
+  // case, always return true when the Error was initialized as a string
+  // pointer, and false when it was initialized as an enum, where the
+  // constructor sets the value of pad_.back() to 1.
+  // While this assumption is very likely to hold for most 64-bit OSs, it might
+  // not be true on 32-bit, so the design is not especially portable, and more
+  // of an exercise in over-engineering for fun :)
+  bool IsString() const { return pad_.back() == 0; }
 };
-
-inline Error::ErrorCode Error::code() const {
-  if (code_ != IS_DETAILED_ERROR_) {
-    return code_;
-  }
-  const DetailedError* detailed = static_cast<const DetailedError*>(this);
-  return detailed->code();
-}
 
 }  // namespace leveldb
