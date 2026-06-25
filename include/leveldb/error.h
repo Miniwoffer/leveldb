b2361b6249af66a76e67ef53c922c48b38f340ba
Ingar Solveigson Asheim <isa@applica.no>
2026-06-25 15:10:07 +0200

    Error: Enum version with static message map

--- include/leveldb/error.h
+++ include/leveldb/error.h
@@ -1,9 +1,10 @@
 
-#include <array>
+#include <atomic>
 #include <cassert>
+#include <concepts>
 #include <cstdint>
-#include <memory>
-#include <optional>
+#include <map>
+#include <mutex>
 #include <string>
 
 #include "leveldb/export.h"
@@ -11,28 +12,28 @@
 namespace leveldb {
 
 namespace {
-constexpr const size_t PTR_SZ = sizeof(std::unique_ptr<std::string>);
-
-consteval auto EnumBaseType() {
-  if constexpr (PTR_SZ == 8) {
-    return uint_least32_t{};
+consteval auto CodeBaseType() {
+  constexpr const size_t ptr_sz = sizeof(size_t);
+  if constexpr (ptr_sz == 8) {
+    return uint32_t{};
+  } else if constexpr (ptr_sz == 4) {
+    return uint16_t{};
   } else {
-    return []() { static_assert(PTR_SZ == 8, "Unsupported pointer size"); }();
+    return unsigned{};
   }
 }
 
-using enum_base_type = decltype(EnumBaseType());
+using code_t = decltype(CodeBaseType());
 
 template <typename T>
 concept is_string_like =
     std::same_as<std::string, T> || std::same_as<std::string_view, T> ||
-    std::same_as<const char*, T>;  // Unsure if we should allow const char*
-
+    std::same_as<const char*, T> || std::same_as<const char&, T>;
 }  // namespace
 
 class LEVELDB_EXPORT Error {
  public:
-  enum class Code : enum_base_type {
+  enum class Code : code_t {
     NotFound = 1,
     Corruption = 2,
     NotSupported = 3,
@@ -40,37 +41,56 @@ class LEVELDB_EXPORT Error {
     IOError = 5,
   };
 
+  Error(Code c) noexcept : code_(c) {}
+
   template <typename... Args>
     requires(is_string_like<Args> && ...)
-  Error(Code c, Args... args) {
+  Error(Code c, Args... args) : code_(c), msg_key_(GetNextKey()) {
     std::string_view messages[] = {args...};
-    string_ = std::make_unique<std::string>();
-    string_->push_back(static_cast<char>(c));
+    std::string message;
     for (const auto& m : messages) {
-      string_->append(m.data(), m.size());
-      string_->append(": ", 2);
+      message.append(m);
+      message.append(": ");
     }
-    string_->resize(string_->size() - 2);  // Remove last ": "
+    message.resize(message.size() - 2);  // Remove last ": "
+    SetMessage(std::move(message));
   }
-
-  Error(Code c) : code_(c) { pad_.back() = 1; }
 
   ~Error() {
-    if (IsString()) {
-      string_.reset();
+    if (HasMessage()) {
+      EraseMessage();
     }
   }
 
-  Code code() const {
-    return IsString() ? static_cast<Code>((*string_)[0]) : code_;
+  Error(const Error& other) { *this = other; }
+  Error& operator=(const Error& other) {
+    if (HasMessage()) {
+      EraseMessage();
+    }
+    code_ = other.code_;
+    // std::map::erase does nothing if no element with the provided key
+    // exists, so multiple Errors calling erase on the same key is safe,
+    // and this prevents creating copies of the same error message.
+    msg_key_ = other.msg_key_;
+    return *this;
   }
 
-  bool operator==(const Code rhs) { return code() == rhs; }
-  bool operator==(const Error& rhs) { return code() == rhs.code(); }
+  Error(Error&& other) noexcept { *this = std::move(other); }
+  Error& operator=(Error&& other) noexcept {
+    if (this != &other) {
+      code_ = other.code_;
+      msg_key_ = other.msg_key_;
+      other.code_ = static_cast<Code>(0);
+      other.msg_key_ = 0;
+    }
+    return *this;
+  }
+
+  bool operator==(const Code rhs) const { return code_ == rhs; }
 
   std::string ToString() const {
     std::string error_string;
-    switch (code()) {
+    switch (code_) {
       case Code::NotFound:
         error_string = "Not found";
         break;
@@ -90,40 +110,54 @@ class LEVELDB_EXPORT Error {
         return "Invalid error code";
     }
 
-    if (!IsString()) {
-      return error_string;
+    if (HasMessage()) {
+      error_string.append(": ");
+      error_string.append(GetMessage());
     }
 
-    error_string.append(": ");
-    error_string.append(*Message());
     return error_string;
   }
 
  private:
-  union {
-    std::unique_ptr<std::string> string_{nullptr};
-    struct {
-      Code code_;
-      std::array<char, PTR_SZ - sizeof(code_)> pad_;
-    };
+  struct {
+    Code code_{0};
+    code_t msg_key_{0};
   };
 
-  std::optional<std::string> Message() const {
-    if (IsString()) {
-      return string_->substr(1, string_->size() - 1);
-    }
-    return std::nullopt;
+  inline static std::atomic<code_t> next_key_{1};
+  inline static std::mutex msg_mu_;
+  inline static std::map<code_t, std::string> messages_;
+
+  // Can deliberately overflow
+  code_t GetNextKey() {
+    code_t z = 0, o = 1;
+    next_key_.compare_exchange_strong(z, o);
+    code_t key = next_key_.fetch_add(1);
+    assert(!messages_.contains(key));
+    return key;
   }
 
-  // Relies on user space addresses always being in [0, 2^48), which means bytes
-  // 7 and 8 of a user space pointer will always be 0. This check will, in this
-  // case, always return true when the Error was initialized as a string
-  // pointer, and false when it was initialized as an enum, where the
-  // constructor sets the value of pad_.back() to 1.
-  // While this assumption is very likely to hold for most 64-bit OSs, it might
-  // not be true on 32-bit, so the design is not especially portable, and more
-  // of an exercise in over-engineering for fun :)
-  bool IsString() const { return pad_.back() == 0; }
+  bool HasMessage() const { return msg_key_ != 0; }
+
+  const std::string& GetMessage() const {
+    // const version of .at() is thread-safe
+    return messages_.at(msg_key_);
+  }
+
+  void SetMessage(const std::string& msg) const {
+    std::lock_guard<std::mutex> guard(msg_mu_);
+    messages_[msg_key_] = msg;
+  }
+
+  void SetMessage(const std::string&& msg) const {
+    std::lock_guard<std::mutex> guard(msg_mu_);
+    messages_[msg_key_] = std::move(msg);
+  }
+
+  void EraseMessage() const {
+    std::lock_guard<std::mutex> guard(msg_mu_);
+    messages_.erase(msg_key_);
+  }
 };
 
 }  // namespace leveldb
