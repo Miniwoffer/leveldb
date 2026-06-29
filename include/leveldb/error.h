a3f3ee395c5361c903864940ff1a111ac5ee7032
Ingar Solveigson Asheim <isa@applica.no>
2026-06-29 12:46:32 +0200

    Error: Adressed PR comments

--- include/leveldb/error.h
+++ include/leveldb/error.h
@@ -1,5 +1,7 @@
+#pragma once
 
 #include <algorithm>
+#include <array>
 #include <atomic>
 #include <cassert>
 #include <cstdint>
@@ -13,7 +15,7 @@
 namespace leveldb {
 
 namespace {
-consteval auto CodeBaseType() {
+static consteval auto CodeBaseType_() {
   constexpr const size_t ptr_sz = sizeof(size_t);
   if constexpr (ptr_sz == 8) {
     return uint32_t{};
@@ -23,19 +25,24 @@ consteval auto CodeBaseType() {
     return unsigned{};
   }
 }
-using code_t = decltype(CodeBaseType());
 }  // namespace
 
 class LEVELDB_EXPORT Error {
+  using code_t = decltype(CodeBaseType_());
+
  public:
   enum class Code : code_t {
-    Ok = 0,  // TODO: until everything is expected returns semantics
+    Ok = 0,  // TODO: deprecate after everything is expected returns semantics
     NotFound = 1,
     Corruption = 2,
     NotSupported = 3,
     InvalidArgument = 4,
     IOError = 5,
+    _Count
   };
+
+  // TODO: deprecate after everything is expected returns semantics
+  Error() noexcept {}
 
   Error(Code c) noexcept : code_(c) {}
 
@@ -55,19 +62,8 @@ class LEVELDB_EXPORT Error {
     }
   }
 
-  Error(const Error& other) { *this = other; }
-  Error& operator=(const Error& other) {
-    if (this != &other) {
-      if (HasMessage()) {
-        DeleteMessage();
-      }
-      code_ = other.code_;
-      msg_key_ = other.msg_key_;
-      // DeleteMessage() is safe to call repeatedly on the same key, so this
-      // saves copying the message
-    }
-    return *this;
-  }
+  Error(const Error& other) = delete;
+  Error& operator=(const Error& other) = delete;
 
   Error(Error&& other) noexcept { *this = std::move(other); }
   Error& operator=(Error&& other) noexcept {
@@ -87,27 +83,67 @@ class LEVELDB_EXPORT Error {
 
   static Error NotFound(std::string_view msg,
                         std::string_view msg2 = std::string_view()) {
-    return Error(Error::Code::NotFound, msg, msg2);
+    bool has_msg = msg.size() > 0;
+    bool has_msg2 = msg2.size() > 0;
+    if (has_msg && has_msg2) {
+      return Error(Error::Code::NotFound, msg, msg2);
+    } else if (has_msg && !has_msg2) {
+      return Error(Error::Code::NotFound, msg);
+    } else {
+      return Error(Error::Code::NotFound);
+    }
   }
 
   static Error Corruption(std::string_view msg,
                           std::string_view msg2 = std::string_view()) {
-    return Error(Error::Code::Corruption, msg, msg2);
+    bool has_msg = msg.size() > 0;
+    bool has_msg2 = msg2.size() > 0;
+    if (has_msg && has_msg2) {
+      return Error(Error::Code::Corruption, msg, msg2);
+    } else if (has_msg && !has_msg2) {
+      return Error(Error::Code::Corruption, msg);
+    } else {
+      return Error(Error::Code::Corruption);
+    }
   }
 
   static Error NotSupported(std::string_view msg,
                             std::string_view msg2 = std::string_view()) {
-    return Error(Error::Code::NotSupported, msg, msg2);
+    bool has_msg = msg.size() > 0;
+    bool has_msg2 = msg2.size() > 0;
+    if (has_msg && has_msg2) {
+      return Error(Error::Code::NotSupported, msg, msg2);
+    } else if (has_msg && !has_msg2) {
+      return Error(Error::Code::NotSupported, msg);
+    } else {
+      return Error(Error::Code::NotSupported);
+    }
   }
 
   static Error InvalidArgument(std::string_view msg,
                                std::string_view msg2 = std::string_view()) {
-    return Error(Error::Code::InvalidArgument, msg, msg2);
+    bool has_msg = msg.size() > 0;
+    bool has_msg2 = msg2.size() > 0;
+    if (has_msg && has_msg2) {
+      return Error(Error::Code::InvalidArgument, msg, msg2);
+    } else if (has_msg && !has_msg2) {
+      return Error(Error::Code::InvalidArgument, msg);
+    } else {
+      return Error(Error::Code::InvalidArgument);
+    }
   }
 
   static Error IOError(std::string_view msg,
                        std::string_view msg2 = std::string_view()) {
-    return Error(Error::Code::IOError, msg, msg2);
+    bool has_msg = msg.size() > 0;
+    bool has_msg2 = msg2.size() > 0;
+    if (has_msg && has_msg2) {
+      return Error(Error::Code::IOError, msg, msg2);
+    } else if (has_msg && !has_msg2) {
+      return Error(Error::Code::IOError, msg);
+    } else {
+      return Error(Error::Code::IOError);
+    }
   }
 
   bool operator==(const Code rhs) const { return code_ == rhs; }
@@ -120,57 +156,45 @@ class LEVELDB_EXPORT Error {
   bool IsInvalidArgument() const { return code_ == Code::InvalidArgument; }
 
   std::string ToString() const {
-    std::string error_string;
-    switch (code_) {
-      case Code::NotFound:
-        error_string = "Not found";
-        break;
-      case Code::Corruption:
-        error_string = "Corruption";
-        break;
-      case Code::NotSupported:
-        error_string = "Not implemented";
-        break;
-      case Code::InvalidArgument:
-        error_string = "Invalid argument";
-        break;
-      case Code::IOError:
-        error_string = "IO error";
-        break;
-      default:
-        return "Invalid error code";
-    }
-
+    std::string error_string{GetCodeString()};
     if (HasMessage()) {
       error_string.append(": ");
-      error_string.append(std::move(GetMessage()));
+      error_string.append(GetMessage());
     }
-
     return error_string;
   }
 
  private:
-  struct {
-    Code code_{0};
-    code_t msg_key_{0};
-  };
+  Code code_{0};
+  code_t msg_key_{0};
 
-  inline static std::atomic<code_t> next_key_{1};
-  inline static std::mutex msg_mu_;
-  inline static std::map<code_t, std::string> messages_;
+  static inline const std::array<std::string_view,
+                                 static_cast<size_t>(Code::_Count)>
+      code_strings_ = {"Ok", "Not found", "Corruption", "Invalid argument",
+                       "IO error"};
+
+  static inline std::atomic<code_t> next_key_{1};
+  static inline std::mutex msg_mu_;
+  static inline std::map<code_t, std::string> messages_;
 
   // Can deliberately overflow
   code_t GetNextKey() const {
-    code_t z = 0, o = 1;
-    next_key_.compare_exchange_strong(z, o);
     code_t key = next_key_.fetch_add(1);
+    if (key == 0) {
+      key = next_key_.fetch_add(1);
+    }
     assert(!messages_.contains(key));
     return key;
   }
 
   bool HasMessage() const { return msg_key_ != 0; }
 
-  std::string GetMessage() const { return messages_.at(msg_key_); }
+  std::string_view GetCodeString() const {
+    return (code_ < Code::_Count) ? code_strings_[static_cast<size_t>(code_)]
+                                  : "Unknown error code";
+  }
+
+  std::string_view GetMessage() const { return messages_.at(msg_key_); }
 
   void SetMessage(std::string&& msg) const {
     std::lock_guard<std::mutex> guard(msg_mu_);
